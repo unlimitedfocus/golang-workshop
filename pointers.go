0557bf33187f6b964118a18b3fcf45b890aa08f2
JungChoon Park <unlimitedfocus@gmail.com>
2017-11-30 00:43:34 +0900

    pointers

--- pointers.go
+++ pointers.go
@@ -1,5 +1,9 @@
 package workshop
 
+import (
+	"math"
+)
+
 type avg []float64
 
 func (nums *avg) average() float64 {
@@ -7,21 +11,33 @@ func (nums *avg) average() float64 {
 	for _, num := range *nums {
 		sum += num
 	}
-	return sum / float64(len((*nums)))
+	return round(sum / float64(len((*nums))), 2)
+}
+
+func round(val float64, prec int) float64 {
+	var rounder float64
+	intermed := val * math.Pow(10, float64(prec))
+
+	if val >= 0.5 {
+		rounder = math.Ceil(intermed)
+	} else {
+		rounder = math.Floor(intermed)
+	}
+	return rounder / math.Pow(10, float64(prec))
 }
 
 func pointers() {
 	i, j := 3, 4
 	var a = &i
 	var b = &j
-	assert(i == *b)
-	assert(j == *a)
+	assert(i == *a)
+	assert(j == *b)
 
 	*a = 10
-	assert(i == *b)
+	assert(i == *a)
 
 	var num *int
 	assert(num == nil)
 	var numbers = avg{2.3, 3.5, 7.5, 2.5, 7.8}
-	assert(numbers.average() == 0.0)
+	assert(numbers.average() == 4.73)
 }
