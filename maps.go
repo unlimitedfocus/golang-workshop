9fa2c9d174a74bbf40f34dcec1d857ff7702f733
JungChoon Park <unlimitedfocus@gmail.com>
2017-11-30 23:03:27 +0900

    maps

--- maps.go
+++ maps.go
@@ -7,7 +7,7 @@ import (
 func maps() {
 	// declare and initialize map in one line
 	var intmap = map[string]int{"one": 1, "two": 2, "three": 3}
-	var one = intmap["one"]
+	var one = intmap["two"]
 	assert(one == 2)
 
 	// Use the make function to create an empty map
@@ -16,11 +16,11 @@ func maps() {
 	for i := 1; i <= 10; i++ {
 		strmap[strVal+strconv.Itoa(i)] = strVal + " is the number " + strconv.Itoa(i)
 	}
-	assert(strmap["val3"] == "what am i")
+	assert(strmap["val3"] == "val is the number 3")
 
 	// delete key/value pair from map
 	delete(intmap, "three")
 
 	// len builtin on map reports the number of key/value pairs on map
-	assert(len(intmap) == 3)
+	assert(len(intmap) == 2)
 }
